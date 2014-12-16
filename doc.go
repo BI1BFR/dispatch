089d46bd546261494ee56cf404081b4700b19f4b
huangml <huangml.zh@gmail.com>
2014-12-17 07:55:51 +0800

    add chart

--- doc.go
+++ doc.go
@@ -2,4 +2,32 @@
 //
 // The basic idea is goroutine-per-request, which means each request runs in a
 // separate goroutine.
+//
+//                   +  +  +
+//                   |  |  |
+//                   |  |  | goroutine per request
+//                   |  |  |
+//                   v  v  v
+//          +---------------------+
+//          |      Dispatcher     |
+//          +----------+----------+
+//            +--------+--------+
+//            |        |  +-----|----------------------------------+
+//            v        v  |     v       In-memory entity           |
+//          +----+  +----+|+---------+                             |
+//          |Dest|  |Dest|||  Dest   |                             |
+//          +----+  +----+|+---------+                             |
+//                        ||  Mutex  |             +------------+  |
+//                        ||+-------+|   access    |            |  |
+//                        |||Handler+------------->|            |  |
+//                        ||+-------+|sequentially | Associated |  |
+//                        ||+Handler+------------->|            |  |
+//                        ||+-------+|             | Resources  |  |
+//                        ||+Handler+------------->|            |  |
+//                        ||+-------+|             |            |  |
+//                        |+---------+             +------------+  |
+//                        |                                        |
+//                        +----------------------------------------+
+//
+//
 package dispatch
